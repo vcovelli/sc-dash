9b4bd3a717c24fc0c81f5d8b16e4651b87bf4626
Vincent Covelli <182012017+vcovelli@users.noreply.github.com>
2025-04-11 00:28:42 -0400

    Fine tuned dependecy installation to dockerfile, removed it from entry script, fixed log permission issue, and updated depreciated airflow db init to airflow db migrate. Updated README

--- airflow/airflow_entrypoint.sh
+++ airflow/airflow_entrypoint.sh
@@ -6,9 +6,8 @@ set -a
 source /app/.env
 set +a
 
-# Install DAG-specific Python packages
-echo "Installing Airflow requirements..."
-pip install --no-cache-dir -r /opt/airflow/airflow-requirements.txt
+# Fix log folder permissions
+chown -R airflow: /opt/airflow/logs
 
 # Wait for PostgreSQL
 until nc -z "$AIRFLOW_DB_HOST" "$AIRFLOW_DB_PORT"; do
@@ -17,7 +16,7 @@ until nc -z "$AIRFLOW_DB_HOST" "$AIRFLOW_DB_PORT"; do
 done
 
 # Init DB
-airflow db init
+airflow db migrate
 
 # Create admin user (only if it doesn't exist)
 airflow users create \
