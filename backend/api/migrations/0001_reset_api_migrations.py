b59bc2f7459a59e0b74edf614401874069556cc4
vcovelli <182012017+vcovelli@users.noreply.github.com>
2025-07-09 18:50:37 +0000

    (backend) Had to reset DB migrations.

--- backend/api/migrations/0001_reset_api_migrations.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-
-    dependencies = []
-
-    operations = [
-        migrations.RunSQL("DELETE FROM django_migrations WHERE app = 'api';")
-    ]
