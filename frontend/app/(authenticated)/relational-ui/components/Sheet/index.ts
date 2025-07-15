de17c39a1dc77cbfc87dd80c5d3f46c9c4889edc
vcovelli <182012017+vcovelli@users.noreply.github.com>
2025-07-15 20:11:22 +0000

    (frontend) Linting & Debugging 'relational-ui' page.

--- frontend/app/(authenticated)/relational-ui/components/Sheet/index.ts
+++ frontend/app/(authenticated)/relational-ui/components/Sheet/index.ts
@@ -13,12 +13,14 @@ export type ColumnDataType =
   | "number"
   | "link"
   | "attachment"
+  | "email"
   | "formula";
 
 // More strongly typed choice/reference option shape
 export interface Option {
   id: string;
   name: string;
+  isAddNew?: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -29,9 +31,14 @@ export interface CustomColumnDef<TData = Row> {
   id?: string;
   choices?: string[] | Option[];
   referenceData?: Option[];
-  reference_table?: string;
+  referenceTable?: string;
+  referenceDisplayField?: string;
   formula?: string;
   currencyCode?: string;
+
+  width?: number;
+  isVisible?: boolean;
+  isEditable?: boolean;
 
   onAddChoice?: (newName: string, color?: string) => Promise<Option | null> | Option | null;
   onAddReference?: (newName: string) => Promise<Option | null> | Option | null;
